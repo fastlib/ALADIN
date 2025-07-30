5d231e46794fcc97ec073a9e4e35b5bf4a18c372
Lukas Arts <lukas@betal112465.cs.uu.nl>
2025-07-30 14:31:50 +0200

    Added paper benchmark icentia file

---
+++ benchmark_diagnosis_ICENTIA.sh
@@ -0,0 +1,4 @@
+#dataset CINC
+python benchmark_diagnosis.py --method ALADIN --dataset ICENTIA --overwrite
+#python benchmark_diagnosis.py --method Hannun --dataset CINC --overwrite
+#python benchmark_diagnosis.py --method ECGFounder --dataset CINC --overwrite
