9b7c091c26961dd3b985e3fe1ab44813a7f5ad48
root <l.p.a.arts@uu.nl>
2025-08-02 22:35:57 +0000

    Final bugfixes

--- benchmark_diagnosis.py
+++ benchmark_diagnosis.py
@@ -744,8 +744,8 @@ class ALADINModelForICENTIA(ALADINModel):
         
         print("Reflection on CPU done: ", batch_index, flush=True)
         print("Diagnosing records on CPU: ", batch_index, flush=True)
-        for record in recs:
-            logic = LogicEngine(debug=False, customarrhythmia={"NSR": False, "PAC_PVC": False})
+        for record in tqdm(recs):
+            logic = LogicEngine(debug=False, customarrhythmia={"PAC_PVC": False})
             logic.diagnose(record)
 
         print("Processing reflection and diagnosis on CPU done: ", batch_index, flush=True)
